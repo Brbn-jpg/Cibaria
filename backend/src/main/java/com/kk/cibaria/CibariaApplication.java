d556232579c8867d3b5826d0560436f4229b1dfc
GA3RlEL <kruszkagabrys@gmail.com>
2024-12-17 01:32:02 +0100

    Backend initial added

---
+++ backend/src/main/java/com/kk/cibaria/CibariaApplication.java
@@ -0,0 +1,13 @@
+package com.kk.cibaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibariaApplication.class, args);
+	}
+
+}
