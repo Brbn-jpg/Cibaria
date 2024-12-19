83e77bb59fe88adff6500f5c210d9a0b39392d4f
brbn <kubon.kuznicki@gmail.com>
2024-12-19 13:47:29 +0100

    added feature section

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { SliderComponent } from './slider/slider.component';
+import { FeatureSectionComponent } from './feature-section/feature-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, SliderComponent],
+  imports: [
+    RouterOutlet,
+    HeroSectionComponent,
+    SliderComponent,
+    FeatureSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
