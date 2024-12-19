ed3b9fb631954bb3ed5a745f74e0da03f88ba24f
brbn <kubon.kuznicki@gmail.com>
2024-12-19 20:15:56 +0100

    added cta-section
    added call to action section

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -3,6 +3,7 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { SliderComponent } from './slider/slider.component';
 import { FeatureSectionComponent } from './feature-section/feature-section.component';
 import { TestimonialsSectionComponent } from './testimonials-section/testimonials-section.component';
+import { CtaSectionComponent } from './cta-section/cta-section.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,6 +13,7 @@ import { TestimonialsSectionComponent } from './testimonials-section/testimonial
     SliderComponent,
     FeatureSectionComponent,
     TestimonialsSectionComponent,
+    CtaSectionComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
