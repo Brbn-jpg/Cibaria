dc5dc4a0115456bf9fa0e337e36f55473f266815
brbn <kubon.kuznicki@gmail.com>
2024-12-19 02:14:05 +0100

    added slider + minor changes
    Added automatic slider below hero section. Fixed gradient background on hero section + changed hero-img.jpg to hero-img.png in order to make background transparent for cleaner look

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SliderComponent } from './slider/slider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent],
+  imports: [RouterOutlet, HeroSectionComponent, SliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
