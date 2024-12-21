204a6791991ff17ea0201488fc019b0e76eff537
brbn <kubon.kuznicki@gmail.com>
2024-12-21 17:42:41 +0100

    design change
    changed gallery and added animation to it
    added animation on scroll to cta section

--- frontend/src/app/landing-page/cta-section/cta-section.component.ts
+++ frontend/src/app/landing-page/cta-section/cta-section.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cta-section',
   standalone: true,
   imports: [],
   templateUrl: './cta-section.component.html',
-  styleUrl: './cta-section.component.css'
+  styleUrl: './cta-section.component.css',
 })
-export class CtaSectionComponent {
-
+export class CtaSectionComponent implements OnInit {
+  ngOnInit(): void {
+    const hiddenElements = document.querySelectorAll(
+      '.hidden'
+    ) as NodeListOf<HTMLElement>;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+    hiddenElements.forEach((element: HTMLElement) => {
+      observer.observe(element);
+    });
+  }
 }
