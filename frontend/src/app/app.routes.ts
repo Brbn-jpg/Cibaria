b37f344f44f3bfe9e5068ebb40510790b66a32c5
brbn <kubon.kuznicki@gmail.com>
2024-12-27 18:47:58 +0100

    added login redirectiong
    added login site without actual login or register for now

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
