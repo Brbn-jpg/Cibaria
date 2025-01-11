2e06f13c6739d5f834591b3307eb8e8262d8509b
brbn <kubon.kuznicki@gmail.com>
2025-01-11 20:15:07 +0100

    fetching recipe data

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+
 import { FooterSectionComponent } from '../footer-section/footer-section.component';
+
+export interface recipesRequest {
+  recipeName: string;
+  difficulty: number;
+  prepareTime: number;
+  servings: number;
+}
+
+export interface tokenResponse {
+  token: string;
+  tokenType: string;
+}
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
   imports: [RouterLink, FooterSectionComponent],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css',
+  styleUrls: ['./recipes.component.css'],
 })
-export class RecipesComponent {}
+export class RecipesComponent implements OnInit {
+  url: string = 'http://localhost:8080/api/recipes';
+  http = inject(HttpClient);
+
+  recipes() {
+    this.http.get<tokenResponse>(this.url).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.recipes();
+  }
+}
