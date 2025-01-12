02f14bd22f56f1258331a80306834a670cd1c266
brbn <kubon.kuznicki@gmail.com>
2025-01-12 14:18:11 +0100

    recipe loading

--- frontend/src/app/recipes/recipes.component.ts
+++ frontend/src/app/recipes/recipes.component.ts
@@ -9,13 +9,13 @@ export interface recipesRequest {
   difficulty: number;
   prepareTime: number;
   servings: number;
+  ratings: rating[];
 }
 
-export interface tokenResponse {
-  token: string;
-  tokenType: string;
+export interface rating {
+  ratingId: number;
+  value: number;
 }
-
 @Component({
   selector: 'app-recipes',
   standalone: true,
@@ -26,17 +26,138 @@ export interface tokenResponse {
 export class RecipesComponent implements OnInit {
   url: string = 'http://localhost:8080/api/recipes';
   http = inject(HttpClient);
+  recipesArray: recipesRequest[] = [];
 
   recipes() {
-    this.http.get<tokenResponse>(this.url).subscribe({
+    this.http.get<recipesRequest[]>(this.url).subscribe({
       next: (response) => {
         console.log(response);
+        // response.forEach((recipe) => this.fillFigure(recipe));
+        this.recipesArray = response;
       },
       error: (err) => {
         console.error(err);
       },
     });
   }
+
+  getAverageRating(ratings: rating[]) {
+    if (ratings.length === 0) {
+      return 0;
+    }
+    return ratings.reduce((x, y) => x + y.value, 0);
+  }
+
+  getDifficulty(difficulty: number) {
+    switch (difficulty) {
+      case 1:
+        return 'easy';
+      case 2:
+        return 'medium';
+      case 3:
+        return 'hard';
+      default:
+        return 'no value';
+    }
+  }
+
+  // fillFigure(data: recipesRequest) {
+  //   console.log(data);
+  //   const figure = document.createElement('figure');
+  //   figure.classList.add('recipe');
+  //   const imageWrapper = document.createElement('div');
+  //   imageWrapper.classList.add('imageWrapper');
+
+  //   const recipeImage = document.createElement('img');
+  //   recipeImage.classList.add('recipe-image');
+  //   recipeImage.src = 'images/gallery/gallery1.webp';
+
+  //   const h2 = document.createElement('h2');
+  //   h2.classList.add('recipe-name');
+  //   h2.innerHTML = data.recipeName;
+
+  //   const recipeDesc = document.createElement('div');
+  //   recipeDesc.classList.add('recipe-desc');
+
+  //   const ratingDiv = document.createElement('div');
+  //   ratingDiv.classList.add('recipe-rating');
+  //   ratingDiv.classList.add('recipe-desc-inner');
+
+  //   const starIcon = document.createElement('img');
+  //   starIcon.classList.add('icon');
+  //   starIcon.src = 'images/icons/star-outline.svg';
+  //   starIcon.alt = 'rating icon';
+
+  //   let averageRating = 'No ratings';
+  //   if (data.ratings && data.ratings.length > 0) {
+  //     const sum = data.ratings.reduce((acc, curr: any) => acc + curr.value, 0);
+  //     averageRating = (sum / data.ratings.length).toFixed(1);
+  //   }
+
+  //   const rating = document.createElement('span');
+  //   rating.innerHTML = averageRating;
+
+  //   const difficultyDiv = document.createElement('div');
+  //   difficultyDiv.classList.add('recipe-difficulty');
+  //   difficultyDiv.classList.add('recipe-desc-inner');
+
+  //   const chartIcon = document.createElement('img');
+  //   chartIcon.classList.add('icon');
+  //   chartIcon.src = 'images/icons/bar-chart-outline.svg';
+  //   chartIcon.alt = 'difficulty icon';
+
+  //   let difficulty = document.createElement('span');
+  //   if (data.difficulty === 1) {
+  //     difficulty.innerHTML = 'easy';
+  //   } else if (data.difficulty === 2) {
+  //     difficulty.innerHTML = 'medium';
+  //   } else if (data.difficulty === 3) {
+  //     difficulty.innerHTML = 'hard';
+  //   }
+
+  //   const servingsDiv = document.createElement('div');
+  //   servingsDiv.classList.add('recipe-servings');
+  //   servingsDiv.classList.add('recipe-desc-inner');
+
+  //   const servingsIcon = document.createElement('img');
+  //   servingsIcon.classList.add('icon');
+  //   servingsIcon.src = 'images/icons/restaurant-outline.svg';
+  //   servingsIcon.alt = 'servings icon';
+
+  //   const servingsSize = document.createElement('span');
+  //   servingsSize.innerHTML = `${data.servings.toString()} servings`;
+
+  //   const prepareTimeDiv = document.createElement('div');
+  //   prepareTimeDiv.classList.add('recipe-time');
+  //   prepareTimeDiv.classList.add('recipe-desc-inner');
+
+  //   const timeIcon = document.createElement('img');
+  //   timeIcon.classList.add('icon');
+  //   timeIcon.src = 'images/icons/time-outline.svg';
+  //   timeIcon.alt = 'prepare time icon';
+
+  //   const time = document.createElement('span');
+  //   time.innerHTML = `${data.prepareTime} minutes`;
+
+  //   figure.appendChild(imageWrapper);
+  //   imageWrapper.appendChild(recipeImage);
+  //   figure.appendChild(h2);
+  //   figure.appendChild(recipeDesc);
+  //   recipeDesc.appendChild(ratingDiv);
+  //   ratingDiv.appendChild(starIcon);
+  //   ratingDiv.appendChild(rating);
+  //   recipeDesc.appendChild(difficultyDiv);
+  //   difficultyDiv.appendChild(chartIcon);
+  //   difficultyDiv.appendChild(difficulty);
+  //   recipeDesc.appendChild(servingsDiv);
+  //   servingsDiv.appendChild(servingsIcon);
+  //   servingsDiv.appendChild(servingsSize);
+  //   recipeDesc.appendChild(prepareTimeDiv);
+  //   prepareTimeDiv.appendChild(timeIcon);
+  //   prepareTimeDiv.appendChild(time);
+  //   document.querySelector('.recipes.grid-4-cols')?.appendChild(figure);
+  //   console.log(figure);
+  // }
 
   ngOnInit(): void {
     this.recipes();
